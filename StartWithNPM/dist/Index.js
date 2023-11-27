c7f70173925751c5f836a8bd72105a9ecbe87f9c
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2023-11-26 17:52:52 -0700

    Start

---
+++ StartWithNPM/dist/Index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Tik-Tak-Toe');
+let arrayTTT = ["X", " ", " ", " ", "X", " ", " ", " ", "X"];
+arrayTTT.forEach(element => {
+    console.log(element);
+});
+//# sourceMappingURL=Index.js.map
---
+++ StartWithNPM/src/Index.ts
@@ -0,0 +1,8 @@
+console.log('Tik-Tak-Toe');
+
+let arrayTTT: string[] = 
+[ "X"," "," ", " ","X"," "," "," ","X"];
+
+arrayTTT.forEach(element => {
+    console.log(element);
+});
